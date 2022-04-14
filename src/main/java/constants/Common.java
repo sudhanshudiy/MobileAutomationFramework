7d650194769f3b8c55b80204f6b0c6134a5f4ff1
sudhanshudiy <sudhanshu@diy.org>
2022-04-14 16:07:54 +0530

    commited changes

---
+++ src/main/java/constants/Common.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Common {
+        public static final String ANDROID = "android";
+        public static final String CONFIG_PATH = "src/main/resources/config.properties";
+        public static final String DEVICE_NAME = "deviceName";
+        public static final String UIAUTOMATOR = "Uiautomator2";
+        public static final String DIY_APK = "app-release.apk";
+        public static final String DEMO = "ApiDemos-debug.apk";
+        public static final String LOCAL_HOST = "http://127.0.0.1:4723/wd/hub";
+        public static final String PIXEL_EMULATOR = "emulator-5556";
+        public static final String NEXUS_EMULATOR = "emulator-5554";
+        public static final String APP_PACKAGE = "com.diy.develop";
+        public static final String APP_ACTIVITY = "com.diy.MainActivity";
+}
---
+++ src/test/java/ValidateLogin.java
@@ -0,0 +1,23 @@
+import Factories.Base.BaseTest;
+import Factories.DriverFactory;
+import Pages.LoginPage;
+import constants.DriverTypes;
+import io.appium.java_client.AppiumDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ValidateLogin extends BaseTest {
+
+    private AppiumDriver androidDriver;
+
+    @BeforeClass
+    public void startDriver() {
+        androidDriver = DriverFactory.initDriver(DriverTypes.ANDROID);
+    }
+
+    @Test(description = "Launch DIY app and verify home page", groups = "regression", alwaysRun = true)
+    public void verifyHomePage() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(androidDriver);
+        loginPage.login("","");
+    }
+}

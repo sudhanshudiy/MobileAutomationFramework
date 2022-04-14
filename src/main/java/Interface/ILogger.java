5194db1e07edb404319d6632ecb6868376ace75b
sudhanshudiy <sudhanshu@diy.org>
2022-04-14 16:04:33 +0530

    Revert "init commit"
    This reverts commit 4fe711dd8bc9ab4a1a1a12255f96cf28e1993aaa.

--- src/main/java/Interface/ILogger.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public interface ILogger {
-
-    Logger log = LoggerFactory.getLogger(Class.class);
-}
--- src/main/java/Listeners/MobileListener.java
+++
@@ -1,8 +0,0 @@
-package Listeners;
-
-import org.testng.*;
-
-public class MobileListener implements ITestNGListener, ITestListener, IAnnotationTransformer, ISuiteListener, IReporter {
-
-
-}

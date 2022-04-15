b07a95b0021a463af9576db94bf81c61227c6f4b
sudhanshudiy <sudhanshu@diy.org>
2022-04-15 15:47:44 +0530

    updated test cases

--- src/main/java/Listeners/MobileListener.java
+++ src/main/java/Listeners/MobileListener.java
@@ -1,8 +1,68 @@
 package Listeners;
 
+import Interface.ILogger;
 import org.testng.*;
+import org.testng.annotations.ITestAnnotation;
+import org.testng.xml.XmlSuite;
 
-public class MobileListener implements ITestNGListener, ITestListener, IAnnotationTransformer, ISuiteListener, IReporter {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Set;
+
+public class MobileListener implements ITestNGListener, ITestListener, IAnnotationTransformer, ISuiteListener, IReporter, ILogger{
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        log.info(" starting suite ...........::::" + result.getName() + " and started at in milliSeconds ::" + result.getStartMillis());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        String testName = result.getName();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        log.info("total test cases skipped :::" + result.getSkipCausedBy().size());
+        Set<ITestResult> iTestResultSet = result.getTestContext().getSkippedTests().getAllResults();
+        if (!iTestResultSet.isEmpty()){
+            iTestResultSet.forEach(System.out::println);
+        }
+    }
 
 
+    @Override
+    public void onFinish(ISuite suite) {
+        log.info("Finishing test case for the test suite.......:::: " + suite.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+    }
+
+    @Override
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        xmlSuites.forEach(System.out::println);
+    }
+
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        if (isTestConfigured(annotation))
+            annotation.setAlwaysRun(true);
+        annotation.setRetryAnalyzer(RetryAnalyse.class);
+    }
+
+
+    /**
+     * @param iTestAnnotation
+     * @return
+     */
+    private boolean isTestConfigured(ITestAnnotation iTestAnnotation){
+        return iTestAnnotation.getAlwaysRun();
+    }
 }
--- src/main/java/Util/CommonUtils.java
+++ src/main/java/Util/CommonUtils.java
@@ -1,9 +1,20 @@
 package Util;
 
+import io.appium.java_client.AppiumDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
 import java.io.IOException;
 
 public class CommonUtils {
 
+    /**
+     * @param machineName
+     * @return
+     * @throws IOException
+     */
     public Process getProcess(String machineName) throws IOException {
         String cmd = "$ANDROID_HOME/platform-tools/adb devices";
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -13,5 +24,15 @@ public class CommonUtils {
             processBuilder.command("bash", "-c", cmd);
         Process process = processBuilder.start();
         return process;
+    }
+
+    /**
+     * Create Screenshots
+     * @param name
+     * @throws IOException
+     */
+    public static void getScreenshot(String name, AppiumDriver driver) throws IOException {
+        File file  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File(System.getProperty("user.dir")+ "\\" + name+".jpg"));
     }
 }
